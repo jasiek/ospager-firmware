45804e33899cbdb843b8dacfce02b8c9c3460f0b
Jan Szumiec <jan.szumiec@gmail.com>
2024-02-29 17:29:14 +0100

    Nearly there.

--- src/MultiAddressPagerClient.h
+++ src/MultiAddressPagerClient.h
@@ -8,6 +8,7 @@ extern void PagerClientReadBit(void);
 
 class MultiAddressPagerClient : public PagerClient {
 public:
+  MultiAddressPagerClient(PhysicalLayer* phy) : PagerClient(phy) {}
   int16_t startReceive(uint32_t pin, uint32_t *addresses, uint8_t length);
   int16_t readData(uint8_t *data, size_t *len, uint32_t *addr);
 
